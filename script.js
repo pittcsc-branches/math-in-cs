e21e07e0f3f9833fa1c4885f49a9cc223f0f5576
Zekology <71137244+dta12@users.noreply.github.com>
2022-01-24 03:13:09 -0500

    maintain dark mode functionality

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function darkMode() {
-    var bod = document.body;
-    bod.classList.toggle("dark-mode");
+let dark_val = localStorage.getItem("darktoggle");
+
+const onDark = () => {
+  document.body.classList.add("dark-mode");
+  localStorage.setItem("darktoggle", "right");
+}
+
+const offDark = () => {
+  document.body.classList.remove("dark-mode");
+  localStorage.setItem("darktoggle", "left");
+}
+
+const toggle = document.getElementById("darkMode");
+
+if(dark_val === 'right') {
+  onDark();
+  toggle.checked = true;
+}
+
+toggle.addEventListener('change', () => {
+  dark_val = localStorage.getItem("darktoggle");
+  if(toggle.checked){
+    onDark();
+  } else {
+    offDark();
   }
+})
